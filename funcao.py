651585a2dd68ba5388f2957158b31cc4984abd3c
Angevane <ladutra@gmail.com>
2026-01-21 19:20:48 -0300

    Define e utiliza função com retorno

---
+++ funcao.py
@@ -0,0 +1,9 @@
+# Função que recebe dois números e retorna a soma
+def somar(x, y):
+    return x + y
+
+# Chamada da função
+resultado = somar(5, 7)
+
+# Exibe o retorno da função
+print("Resultado:", resultado)
