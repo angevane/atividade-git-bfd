00fb6df6a643278463cf99f241bcdd7c8fcc4d20
Angevane <ladutra@gmail.com>
2026-01-21 19:19:40 -0300

    Exemplos de for e while

---
+++ estrutura_repeticao.py
@@ -0,0 +1,16 @@
+# Usa for para somar números de 1 a 5
+soma = 0
+for i in range(1, 6):
+    soma += i
+
+print("Soma:", soma)
+
+# Usa while para construir uma lista
+numeros = []
+contador = 1
+
+while contador <= 5:
+    numeros.append(contador)
+    contador += 1
+
+print("Lista:", numeros)
